746b22bfa791c34ea9e043f98976f98aab2d7828
Hyunsuk Ju <wngustjr1306@naver.com>
2025-10-17 07:23:11 +0900

    22

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,15 +1,79 @@
 import "./App.css";
+import { useState, useRef, useReducer } from "react";
 import Editor from "./components/Editor";
 import Header from "./components/Header";
 import List from "./components/List";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "JAVA 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "DB 공부하기",
+    date: new Date().getTime(),
+  },
+];
+
+function todosReducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.isDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [todos, todosDispatch] = useReducer(todosReducer, mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    todosDispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  };
+
+  const onUpdate = (targetId) => {
+    todosDispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  };
+
+  const onDelete = (targetId) => {
+    todosDispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
-      안녕하세요~~
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
     </div>
   );
 }
